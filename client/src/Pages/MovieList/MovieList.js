e8d5978e82e390c24f1fa9b52365f6e770b6bfb0
SACHIN SINGH <92503784+sachinsingh3232@users.noreply.github.com>
2024-04-01 22:34:28 +0530

    Update MovieList.js

--- client/src/Pages/MovieList/MovieList.js
+++ client/src/Pages/MovieList/MovieList.js
@@ -48,7 +48,7 @@ const MovieList = () => {
                     MovieApp
                 </div>
                 <div id="Search">
-                    <input type="search" placeholder="Search" value={searchQuery} onChange={onTextChange} />
+                    <input id="search_input" type="search" placeholder="Search" value={searchQuery} onChange={onTextChange} />
                 </div>
                 <img onClick={logout} className="profile" src="https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=" alt="" title={user?.user?.name} />
             </div>
